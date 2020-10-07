c1b063fa3bc91aa2c63d5372def7a2a6b6c5d96b
Shrinath Apake <shreeapke@rediffmail.com>
2020-10-08 00:09:27 +0530

    config change

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,11 +7,7 @@ mongoose.connect(process.env.MONGODB_URL, {
     useFindAndModify: false
 })
 
-// mongoose.connect("mongodb+srv://taskapp:Password123@cluster0.vx4iu.mongodb.net/live-classroom?retryWrites=true", {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false
-// })
+
 
 
 
